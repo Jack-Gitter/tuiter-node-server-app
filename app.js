14e46d6657a19d6d0264350b78b01fdb28ad07c2
Jack Gitter <gitter.j@husky.neu.edu>
2023-03-27 15:40:13 -0400

    tuiter api done

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import express from 'express'; 
 import HelloController from './controllers/hello-controller.js';
 import UserController from './controllers/users/users-controller.js';
+import tuitsController from './controllers/tuits/tuits-controller.js';
 
 const app = express();
 app.use(express.json())
 
 HelloController(app);
 UserController(app);
+tuitsController(app);
 
 app.listen(4000);
