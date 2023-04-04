948b89bde19ae6f6533d6c8b25254cbc81e52938
Jack Gitter <gitter.j@husky.neu.edu>
2023-04-04 16:58:00 -0400

    a9

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ import cors from 'cors'
 import HelloController from './controllers/hello-controller.js';
 import UserController from './controllers/users/users-controller.js';
 import tuitsController from './controllers/tuits/tuits-controller.js';
+import mongoose from "mongoose"
 
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/tuiter')
 
 const app = express();
 app.use(cors());
